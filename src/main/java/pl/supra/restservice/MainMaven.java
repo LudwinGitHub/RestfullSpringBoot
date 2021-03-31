f036c862901511479ad9562a5dbff51daf56a36f
Mateusz Ludwin <supra@MacBook-Air-Mateusz.local>
2021-03-31 16:17:25 +0200

    Add new files

---
+++ src/main/java/pl/supra/restservice/MainMaven.java
@@ -0,0 +1,11 @@
+package pl.supra.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainMaven {
+    public static void main(String[] args) {
+        SpringApplication.run(MainMaven.class, args);
+    }
+}
